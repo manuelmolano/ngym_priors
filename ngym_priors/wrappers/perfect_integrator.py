d3e22d7d51ac2e2687bb318e7d232bb9aed0cdce
manuelmolano <manuelmolanomazon@gmail.com>
2020-10-13 18:35:43 +0200

    fix some issues

--- ngym_priors/wrappers/perfect_integrator.py
+++ ngym_priors/wrappers/perfect_integrator.py
@@ -7,12 +7,12 @@ Created on Tue Jul 28 10:02:28 2020
 """
 
 import neurogym as ngym
-from neurogym.core import TrialWrapper
+from gym import Wrapper
 import numpy as np
-import itertools as it
+from gym import spaces
 
 
-class PerfectIntegrator(TrialWrapper):
+class PerfectIntegrator(Wrapper):
     """
 
     """
@@ -31,25 +31,39 @@ class PerfectIntegrator(TrialWrapper):
         except AttributeError:
             raise AttributeError('task must have attribute choices')
         assert isinstance(self.unwrapped, ngym.TrialEnv), 'Task has to be TrialEnv'
-        self.prev_choice = self.rng.choice(self.n_ch)  # random initialization
-        self.prev_reward = 0
         self.cum_sum = np.zeros((self.n_ch,))
         self.stim_indx = self.env.observation_space.name['stimulus']
         self.obs_sh = self.env.observation_space.shape[0]
+        self.observation_space = spaces.Box(-np.inf, np.inf,
+                                            shape=(self.obs_sh+self.n_ch+1,),
+                                            dtype=np.float32)
+    def reset(self, step_fn=None):
+        if step_fn is None:
+            step_fn = self.step
+        return self.env.reset(step_fn=step_fn)
 
     def step(self, action):
-        if self.env.t_ind >= self.env.start_t['decision']:
+        # get perfect integrator action
+        if self.env.t_ind >= int(self.env.start_t['decision']/self.env.dt):
             act_io = np.argmax(self.cum_sum)+1
         else:
             act_io = 0
-        obs, reward, done, info = self.env.step(act_io)
+        # make action
+        obs, reward_io, done, info = self.env.step(act_io)
+        # if it's the end of trial, store action and reward, and update performance
         if info['new_trial']:
             self.cum_sum = np.zeros((self.n_ch,))
+            reward = 1*(action == info['gt'] and action == act_io)
+            info['performance'] = reward
+            # build observation from previous action and reward
+            onehot = np.zeros((self.n_ch,))
+            onehot[int(act_io)-1] = 1
+            obs_prev_act_rew = np.concatenate((np.zeros((self.obs_sh,)),
+                                               onehot, np.array([reward_io])))
         else:
             self.cum_sum += obs[self.stim_indx]
-        obs = np.array([self.prev_choice, self.prev_reward])
-        obs = np.concatenate((obs, np.zeros((self.obs_sh,))))
-        self.prev_choice = act_io
-        self.prev_reward = reward
-        reward = action == info['gt']
-        return obs, reward, done, info
+            reward = 0
+            obs_prev_act_rew = np.zeros((self.obs_sh+self.n_ch+1,))
+        info['act_io'] = act_io
+        info['reward_io'] = reward_io
+        return obs_prev_act_rew, reward, done, info
